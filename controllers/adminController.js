9bcb4b5721027e83a27307f399943000811b069c
KevenDvorianoff <keven.dvorianoff@etu.univ-lyon1.fr>
2020-03-01 12:41:53 +0100

    ajout recuperation demande

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,16 +3,16 @@ const Admin = require('../models/adminModel')
 exports.Accueil = function(req, res) {
 
     Admin.getRequest().then((results) => {
-        res.render('/admin/accueil', {results})
+        res.render('admin/accueil', {results})
     }).catch((error) => {
         switch(error) {
             case Admin.Errors.NO_RESULTS :
                 norequest = {text : 'Pas de demande'}
-                res.render('/admin/accueil', {norequest})
+                res.render('admin/accueil', {norequest})
                 break;
             default : 
                 alert = {type : 'danger', text : 'Service indisponible'}
-                res.status(503).render('/admin/accueil', {alert})
+                res.status(503).render('admin/accueil', {alert})
                 break;
         }
     });
